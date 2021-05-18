58b75c258e43f440cbcaa1fa399f90487741d34a
Phillip <anicedream2002@gmail.com>
2021-05-18 01:12:22 -0500

    Add Card component & First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Pika from './pika.png'; // 
 
+/*
+    first - do 2 cards
+
+*/
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div id="card">
+          <button><img src={Pika} /></button>
+        </div>
+        <div id="card">
+          <button><img src={Pika} /></button>
+        </div>
+        <div id="card">
+          <button><img src={Pika} /></button>
+        </div>
+        <div id="card">
+          <button><img src={Pika} /></button>
+        </div>
+
+      </div>
     </div>
   );
 }
---
+++ src/components/Card.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import ReactCardFlip from 'react-card-flip'; // import card flip library for simplicity
+
+//styling card
+const CardStyleFront = {
+    boxShadow: "0 0px 20px 0 rgba(0,0,0,0.2)",
+    padding: "20px",
+    margin: "20px",
+    width: "200px",
+    height: "300px",
+    background: "rgba(219, 102, 102, 0.75)"
+}
+
+const CardStyleBack = {
+    boxShadow: "0 0px 20px 0 rgba(0,0,0,0.2)",
+    padding: "20px",
+    margin: "20px",
+    width: "200px",
+    height: "300px",
+}
+
+// create Card component with prop showing the elements in each item
+const Card = ({ project }) => {
+
+    // initially, set flipped to false
+    const [isFlipped, setIsFlipped] = useState(false);
+
+    // render
+    return (
+        // not flipped yet initially
+        // when clicking, set flipped to opposite bool (true -> false, and on the contrary)
+        <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+            <div style={CardStyleFront} onClick={() => setIsFlipped((prev) => !prev)} className="front">
+            </div>
+            <div style={CardStyleBack} onClick={() => setIsFlipped((prev) => !prev)} className="back">
+                <img src={project.img} />
+            </div>
+
+        </ReactCardFlip>
+    )
+}
